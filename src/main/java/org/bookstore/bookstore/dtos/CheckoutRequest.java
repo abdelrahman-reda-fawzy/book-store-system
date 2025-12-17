6f1421aca39a9cba186e90726bb3d8cd04bce545
eyadmohamed20 <mimoeyad14@gmail.com>
2025-12-17 15:52:33 +0200

    adding dto request and query repo for payment

---
+++ src/main/java/org/bookstore/bookstore/dtos/CheckoutRequest.java
@@ -0,0 +1,8 @@
+package org.bookstore.bookstore.dtos;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class CheckoutRequest {
+    private String cardNumber;
+}
---
+++ src/main/java/org/bookstore/bookstore/repositories/BillingInfoRepository.java
@@ -0,0 +1,22 @@
+package org.bookstore.bookstore.repositories;
+import org.bookstore.bookstore.entities.BillingInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillingInfoRepository
+        extends JpaRepository<BillingInfo, Integer> {
+
+    @Query("""
+        SELECT b FROM BillingInfo b
+        WHERE b.user.userID = :userId
+          AND b.cardNumber = :card
+          AND b.expirationDate >= CURRENT_DATE
+    """)
+    BillingInfo validateCard(
+        @Param("userId") Integer userId,
+        @Param("card") String card
+    );
+}
